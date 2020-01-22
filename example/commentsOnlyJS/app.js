b2c602ebd7bd4266f16d17f8fe86fe4ca008a277
paulmreese <paul.m.reese@gmail.com>
2020-01-21 23:33:13 -0500

    Add JS skeleton

--- example/commentsOnlyJS/app.js
+++ example/commentsOnlyJS/app.js
@@ -1,12 +1,42 @@
+const dotenv = require('dotenv');
+
+//setup the variables in the .env file
+dotenv.config();
+
 // Personal API Key for OpenWeatherMap API
+const apiKey = process.env.API_KEY
+
+// API endpoint address
+const apiAddress = 'http://api.openweathermap.org/data/2.5/'
 
 // Event listener to add function to existing HTML DOM element
+document.getElementById("generate").addEventListener("submit", handleSubmit)
 
 /* Function called by event listener */
+/* Thanks to
+ * https://dev.to/shoupn/javascript-fetch-api-and-using-asyncawait-47mp
+ * for better async syntax vs. Promises
+ */
+
+const handleSubmit = async(e) => {
+    e.preventDefault()
+    getWeatherData();
+    saveDataToSever();
+    updateClientFromServer();
+}
 
 /* Function to GET Web API Data*/
+const getWeatherData = () => {
+
+}
 
 /* Function to POST data */
+const saveDataToServer = () => {
+
+}
 
 
 /* Function to GET Project Data */
+const updateClientFromServer = () => {
+
+}
--- example/commentsOnlyJS/server.js
+++ example/commentsOnlyJS/server.js
@@ -1,22 +1,45 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 // Setup empty JS object to act as endpoint for all routes
+let projectData = {}
+
 // Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
 /* Middleware*/
 
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+app.use(cors());
+
 
 // Initialize the main project folder
+app.use(express.static(''));
 
 // Spin up the server
 // Callback to debug
+app.listen(3000, function () {
+    console.log('Listening on port 8080!')
+});
 
 // Initialize all route with a callback function
+app.get('/all', getProjectData)
 
 // Callback function to complete GET '/all'
+getProjectData = (req, res) => {
+    return res.status(200).send(projectData)
+}
 
 // Post Route
-  
+app.post('/all', (req, res) => {
+    { temp, date, userResponse } = req.body
+    projectData[date] = { temp, userResponse }
+    res.status(200).send()
+})
